769642814cfa3bc08cce80c39f71e75d109a2a4a
oskar202 <liblikoskar@gmail.com>
2015-11-25 10:58:42 +0200

    H26

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+app.set('port', (process.env.PORT || 8080))
+
 // set the view engine to jade
 app.set('view engine', 'jade');
 
