b21bf5d2991c79f8f77428c896bee4d5dcbac860
oskar202 <liblikoskar@gmail.com>
2016-01-18 15:43:25 +0200

    H36

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('users');
-  //res.send('respond with a resource');
+  res.send('respond with a resource');
 
 
-
+  
 });
 
 
