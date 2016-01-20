7877759f97a1ba2d0b232fdafdc4c6dc2f29c300
oskar202 <liblikoskar@gmail.com>
2016-01-20 19:11:47 +0200

    H37

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send($('form').serialize());
-
+  res.send('respond with a resource');
+$('form').serialize()
   //siia lehele tulemused
 });
 
