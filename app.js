d86c0f63b12cf348223de8faa63f7e70fe1c498a
oskar202 <liblikoskar@gmail.com>
2016-01-21 16:29:47 +0200

    H37

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var about = require('./routes/about');
 var contact = require('./routes/contact');
-var users = require('./routes/users');
+var flavors = require('./routes/flavors');
 
 
 var app = express();
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/about', about);
 app.use('/contact', contact);
-app.use('/users', users);
+app.use('/flavors', flavors);
 
 
 // catch 404 and forward to error handler
