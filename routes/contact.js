f140f429f2258887a2c81fcc474a9807e3f68ca6
oskar202 <liblikoskar@gmail.com>
2016-01-22 14:44:30 +0200

    ver4.0

--- routes/contact.js
+++ routes/contact.js
@@ -5,7 +5,7 @@ var router = require('express').Router();
 /* GET about page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact',
-						age: '2'
+						title: 'contact'
 													});
 });
 
