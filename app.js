41a067ff9b091df913c74183f4fbc2a5688bae31
oskar202 <liblikoskar@gmail.com>
2016-02-02 21:54:28 +0200

    data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 //var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var random  = require('randomstring')
 var bodyParser = require('body-parser');
 //var mongoose = require('mongoose');
 var stylus = require('stylus')
@@ -12,10 +13,11 @@ var app = express();
 
 
 // serverist copytud
-APP_PORT      = process.env.PORT || 3000;
-APP_ENTU_URL  = process.env.ENTU_URL || 'https://nospel.entu.ee/api2'
-APP_ENTU_USER = process.env.ENTU_USER
-APP_ENTU_KEY  = process.env.ENTU_KEY
+APP_PORT      		= process.env.PORT || 3000;
+APP_ENTU_URL  		= process.env.ENTU_URL || 'https://nospel.entu.ee/api2'
+APP_ENTU_USER 		= process.env.ENTU_USER
+APP_ENTU_KEY  		= process.env.ENTU_KEY
+APP_COOKIE_SECRET   = process.env.COOKIE_SECRET || random.generate(16)
 
 
 // view engine setup
@@ -27,13 +29,56 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+//cookie
+app.use(cookieParser(APP_COOKIE_SECRET));
+
+// set defaults for views
+app.use(function(req, res, next) {
+	res.authenticate = function() {
+		if(!res.locals.user) {
+			res.cookie('redirect_url', '/' + res.locals.path.split('/').slice(2).join('/'), {signed:true, maxAge:1000*60*60})
+			res.redirect('/' + res.locals.lang + '/signin')
+			return false
+		} else {
+			return true
+		}
+	}
+	res.locals.showFeedback = true
+	res.locals.path = req.path
+	if(!req.signedCookies) next(null)
+	if(req.signedCookies.auth_id && req.signedCookies.auth_token) {
+		entu.getUser({
+			auth_id: req.signedCookies.auth_id,
+			auth_token: req.signedCookies.auth_token
+		}, function(error, user) {
+			if(user) {
+				res.locals.user = {
+					id: parseInt(req.signedCookies.auth_id, 10),
+					token: req.signedCookies.auth_token,
+					picture: op.get(user, 'picture'),
+					lang: op.get(user, 'person.language.values.0.value', APP_DEFAULT_LOCALE)
+				}
+			} else {
+				res.clearCookie('auth_id')
+				res.clearCookie('auth_token')
+			}
+			next(null)
+		})
+	} else {
+		next(null)
+	}
+})
+
+
 //app.use(require('stylus-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.use('/', require('./routes/index'));
+app.use('/signin', require('./routes/signin'));
+
 //app.use('/about', require('./routes/about'));
 
 
@@ -50,8 +95,8 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
-    error: app.get('env') === 'development' ? err : {}
+	message: err.message,
+	error: app.get('env') === 'development' ? err : {}
   });
 });
 
