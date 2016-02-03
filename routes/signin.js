c68bea04e3c4acdb125a586e440a39a7ce0d27b4
oskar202 <liblikoskar@gmail.com>
2016-02-03 17:41:46 +0200

    signin

--- routes/signin.js
+++ routes/signin.js
@@ -61,7 +61,7 @@ router.get('/exit', function(req, res) {
 
 // Sign in with given provider
 router.get('/:provider', function(req, res, next) {
-	if(!req.params.provider) res.redirect('/'+ 'signin')
+	if(!req.params.provider) res.redirect('/'+ 'profile')
 
 	res.clearCookie('auth_url')
 	res.clearCookie('auth_state')
@@ -69,7 +69,7 @@ router.get('/:provider', function(req, res, next) {
 	res.clearCookie('auth_token')
 
 	entu.getSigninUrl({
-		redirect_url: req.protocol + '://' + req.hostname + '/' + 'signin/done',
+		redirect_url: req.protocol + '://' + req.hostname + '/' + 'profile',
 		provider: req.params.provider
 	}, function(error, data) {
 		if(error) return next(error)
